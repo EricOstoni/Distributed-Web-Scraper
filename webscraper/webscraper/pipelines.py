d40c8a727284521f889a7bdc18417f8a6946a7d8
EricOstoni <eostoni@student.unipu.hr>
2024-12-16 00:34:05 +0100

    add connection to database

--- webscraper/webscraper/pipelines.py
+++ webscraper/webscraper/pipelines.py
@@ -6,8 +6,30 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import boto3
+from botocore.exceptions import ClientError
 
 
 class WebscraperPipeline:
     def process_item(self, item, spider):
         return item
+    
+    
+#TODO izmjeniti ime tablice i mozda napraviti .env file
+
+class DynamodbPipeline: 
+    def __init__(self):
+        self.dynamodb = boto3.resource("dynamodb" , region_name="croatia") 
+        self.table = self.dynamodb.Table("table") 
+
+    def process_item(self, item, spider): 
+        try: 
+            self.table.put_item(Item={
+                "name" : item.get("name", ""),
+                "price": item.get("price", "")
+            })
+        except ClientError as e:
+            spider.logger.error(f"Error saveing item to database: {e.response['Error']['Message']}")
+        return item
+    
+    
