858672ba8393147a36d7367ecad721cd00be7118
EricOstoni <eostoni@student.unipu.hr>
2025-02-06 19:26:43 +0100

    add spiders

---
+++ webscraper/webscraper/spiders/mac_spider.py
@@ -0,0 +1,33 @@
+import scrapy
+
+class MacSpider(scrapy.Spider): 
+    name = "mac_spider"
+    allowed_domains = ["nabava.net"]
+    start_urls= [
+        "https://www.nabava.net/apple-mac"
+    ]
+    
+
+    def parse(self, response):
+
+        products = response.css(".product")
+
+        for product in products: 
+
+            name = product.css(".product-title::text").get() 
+            price = product.css(".product__price::text").get()
+            link = product.css('a').attrib['href']
+
+
+            yield{
+
+                "name" : name.strip() if name else None,
+                "price" : price.strip() if price else None,
+                "link" : "https://www.nabava.net" + link.strip() if link else None,
+
+            }
+        next_page = response.css("a.next-page::attr(href)").get()
+        if next_page:
+            yield response.follow(next_page, callback=self.parse)
+
+            
