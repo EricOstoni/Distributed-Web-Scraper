0b387869e2571c910cb8212d6c786ee6f52a19b4
EricOstoni <eostoni@student.unipu.hr>
2025-05-18 22:57:39 +0200

    add data structure

--- webscraper/webscraper/items.py
+++ webscraper/webscraper/items.py
@@ -7,6 +7,10 @@ import scrapy
 
 
 class WebscraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
+    id = scrapy.Field()
+    category = scrapy.Field()
+    name = scrapy.Field()
+    price = scrapy.Field()
+    link = scrapy.Field()
+
     pass
