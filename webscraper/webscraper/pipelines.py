aa0e6359c22029d7ec9c99a8d69e5f32497ecb94
EricOstoni <eostoni@student.unipu.hr>
2025-05-18 23:28:05 +0200

    add other spiders in pipeline

--- webscraper/webscraper/pipelines.py
+++ webscraper/webscraper/pipelines.py
@@ -5,14 +5,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class DynamoDBPipeline:
     def __init__(self):
         self.dynamodb = boto3.resource(
-            'dynamodb',
+            "dynamodb",
             endpoint_url=os.getenv("DYNAMODB_ENDPOINT"),
             aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
             aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-            region_name=os.getenv("AWS_REGION")
+            region_name=os.getenv("AWS_REGION"),
         )
         self.table_name = "Products"
 
@@ -22,21 +23,21 @@ class DynamoDBPipeline:
                 TableName=self.table_name,
                 KeySchema=[{"AttributeName": "id", "KeyType": "HASH"}],
                 AttributeDefinitions=[{"AttributeName": "id", "AttributeType": "S"}],
-                ProvisionedThroughput={"ReadCapacityUnits": 5, "WriteCapacityUnits": 5}
+                ProvisionedThroughput={"ReadCapacityUnits": 5, "WriteCapacityUnits": 5},
             )
             self.table.wait_until_exists()
         else:
             self.table = self.dynamodb.Table(self.table_name)
 
     def process_item(self, item, spider):
-        item["id"] = str(uuid.uuid4())  
+        item["id"] = str(uuid.uuid4())
         category_map = {
             "iphone_spider": "iPhone",
             "mac_spider": "Mac",
-            "macbook_spider": "MacBook"
+            "macbook_spider": "MacBook",
+            "ipad_spider": "Ipad",
+            "iwatch_spider": "Apple Watch",
         }
         item["category"] = category_map.get(spider.name, "bo")
         self.table.put_item(Item=dict(item))
         return item
-
-
