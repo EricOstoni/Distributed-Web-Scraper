dccddfa58d27fcf4bde706c454b1a0db2e133fa5
EricOstoni <eostoni@student.unipu.hr>
2025-05-18 22:58:56 +0200

    fix spiders use items for data structure

--- webscraper/webscraper/spiders/iphone_spider.py
+++ webscraper/webscraper/spiders/iphone_spider.py
@@ -1,35 +1,32 @@
 import uuid
 import scrapy
+from ..items import WebscraperItem
 
-class IphoneSpider(scrapy.Spider): 
+
+class IphoneSpider(scrapy.Spider):
     name = "iphone_spider"
     allowed_domains = ["nabava.net"]
-    start_urls= [
-        "https://www.nabava.net/mobiteli?se=1&cod=&cdo="
-    ]
-    
+    start_urls = ["https://www.nabava.net/mobiteli?se=1&cod=&cdo="]
 
     def parse(self, response):
 
         products = response.css(".product")
 
-        for product in products: 
+        for product in products:
 
-            name = product.css(".product-title::text").get() 
+            item = WebscraperItem()
+
+            name = product.css(".product-title::text").get()
             price = product.css(".product__price::text").get()
-            link = product.css('a').attrib['href']
+            link = product.css("a").attrib["href"]
 
+            item["id"] = str(uuid.uuid4())
+            item["category"] = "Iphone"
+            item["name"] = name.strip() if name else None
+            item["price"] = price.strip() if price else None
+            item["link"] = f"https://www.nabava.net{link.strip()}" if link else None
 
-            yield{
-                "id" : str(uuid.uuid4()),
-                "category" : "Iphone",
-                "name" : name.strip() if name else None,
-                "price" : price.strip() if price else None,
-                "link" : "https://www.nabava.net" + link.strip() if link else None,
-
-            }
+            yield item
         next_page = response.css("a.next-page::attr(href)").get()
         if next_page:
             yield response.follow(next_page, callback=self.parse)
-
-            
--- webscraper/webscraper/spiders/mac_spider.py
+++ webscraper/webscraper/spiders/mac_spider.py
@@ -1,35 +1,33 @@
 import uuid
 import scrapy
+from ..items import WebscraperItem
 
-class MacSpider(scrapy.Spider): 
+
+class MacSpider(scrapy.Spider):
     name = "mac_spider"
     allowed_domains = ["nabava.net"]
-    start_urls= [
-        "https://www.nabava.net/apple-mac"
-    ]
-    
+    start_urls = ["https://www.nabava.net/apple-mac"]
 
     def parse(self, response):
 
         products = response.css(".product")
 
-        for product in products: 
+        for product in products:
 
-            name = product.css(".product-title::text").get() 
+            item = WebscraperItem()
+
+            name = product.css(".product-title::text").get()
             price = product.css(".product__price::text").get()
-            link = product.css('a').attrib['href']
+            link = product.css("a").attrib["href"]
 
+            item["id"] = str(uuid.uuid4())
+            item["category"] = "Mac"
+            item["name"] = name.strip() if name else None
+            item["price"] = price.strip() if price else None
+            item["link"] = f"https://www.nabava.net{link.strip()}" if link else None
 
-            yield{
-                "id" : str(uuid.uuid4()),
-                "category" : "Mac",
-                "name" : name.strip() if name else None,
-                "price" : price.strip() if price else None,
-                "link" : "https://www.nabava.net" + link.strip() if link else None,
+            yield item
 
-            }
         next_page = response.css("a.next-page::attr(href)").get()
         if next_page:
             yield response.follow(next_page, callback=self.parse)
-
-            
