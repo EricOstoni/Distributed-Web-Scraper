4edad647afba878cded0192407d6be775dfa9075
EricOstoni <eostoni@student.unipu.hr>
2025-05-23 00:49:52 +0200

    Format file

--- webscraper/webscraper/test/q_price.py
+++ webscraper/webscraper/test/q_price.py
@@ -5,12 +5,14 @@ from decimal import Decimal
 
 load_dotenv()
 
-dynamodb = boto3.resource(  'dynamodb',
-            endpoint_url=os.getenv("DYNAMODB_ENDPOINT"),
-            aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-            aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-            region_name=os.getenv("AWS_REGION")) 
-        
+dynamodb = boto3.resource(
+    "dynamodb",
+    endpoint_url=os.getenv("DYNAMODB_ENDPOINT"),
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+    region_name=os.getenv("AWS_REGION"),
+)
+
 table = dynamodb.Table("Products")
 
 response = table.scan(
@@ -18,10 +20,9 @@ response = table.scan(
     ExpressionAttributeValues={
         ":category": "iPhone",
         ":min": Decimal("500"),
-        ":max": Decimal("1000")
-    }
+        ":max": Decimal("1000"),
+    },
 )
 
-for item in response['Items']:
+for item in response["Items"]:
     print(item)
-
